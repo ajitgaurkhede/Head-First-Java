0b1c86796c56e1a1ba245c3d926b29a0ceb09541
ajitgaurkhede <77144963+ajitgaurkhede@users.noreply.github.com>
2021-01-19 18:28:37 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,39 @@
+
+
+
+class Test{
+
+	Integer i ;
+	
+	int j ;
+
+
+	public static void main(String arg[]){
+	
+	
+	Test t = new Test();
+	
+	t.go();
+	
+	
+		
+	}
+
+	public void go(){
+	
+	//j = i ;
+	
+	i = j;
+	
+	//System.out.println(j);
+	
+	System.out.println(i);
+	
+	
+	
+	}
+
+
+
+}
+
